a0dd2a4c59c3a23754b516309cdaaf2ad4fd632f
Jan Kierejsza <jan.kierejsza@gmail.com>
2023-08-24 09:54:08 +0200

    First session

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router';
+import './App.css';
+import IndexPage from './pages/IndexPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Layout from './Layout';
+import axios from "axios";
+
+axios.defaults.baseURL = 'http://localhost:4000';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<IndexPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
